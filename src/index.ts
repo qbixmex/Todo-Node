3e9561b11991bac07ac4685736b1d73d5873b955
Daniel González Briseño <qbixmex@gmail.com>
2022-12-28 15:24:38 -0600

    eslint-and-prettier

--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,15 @@ dotenv.config();
 //* Define server port
 const PORT = process.env.PORT;
 
+//* Define host
+const HOST = process.env.HOST;
+
 //* Create root endpoint
 app.get('/', (_request: Request, response: Response) => {
-  response.send('Express + Typescript Server');
+  response.send("Express + Typescript Server");
 });
 
 //* Start listening to the request on the defined port
 app.listen(PORT, () => {
-  console.log(`Server Running at: http://localhost:${PORT}`);
+  console.log(`Server Running at: ${HOST}:${PORT}`);
 });
